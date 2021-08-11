31f6360be42a391f78353b361ba21c8a03de202c
周 <zhouyan19@mails.tsinghua.edu.cn>
2021-08-12 00:43:41 +0800

    ADD a readme.

--- app/src/main/java/com/example/catedu/data/DataLoader.java
+++ app/src/main/java/com/example/catedu/data/DataLoader.java
@@ -1,5 +1,13 @@
 package com.example.catedu.data;
 
 public class DataLoader {
-    
+    public void getData () {
+        logIn();
+        HttpClient hc = new HttpClient();
+    }
+
+    public void logIn () {
+
+    }
+
 }
