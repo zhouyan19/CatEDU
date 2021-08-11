65cec9698056239fecef89f9ca7a9416fe7207d9
周 <zhouyan19@mails.tsinghua.edu.cn>
2021-08-11 14:55:57 +0800

    Add an instance class

---
+++ app/src/main/java/com/example/catedu/data/DataLoader.java
@@ -0,0 +1,5 @@
+package com.example.catedu.data;
+
+public class DataLoader {
+    
+}
---
+++ app/src/main/java/com/example/catedu/data/Instance.java
@@ -0,0 +1,15 @@
+package com.example.catedu.data;
+
+public class Instance {
+    String label;
+    String category;
+    String uri;
+
+    public String getLabel () { return label; }
+    public String getCategory () { return category; }
+    public String getUri () { return uri; }
+
+    public void setLabel (String l) { label = l; }
+    public void setCategory (String c) {category = c; }
+    public void setUri (String u) { uri = u; }
+}
