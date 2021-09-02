60f979785029faeb25a2d1dfd4fb45d1a50a9050
周 <zhouyan19@mails.tsinghua.edu.cn>
2021-09-02 15:53:08 +0800

    Fix bug of que-link

--- app/src/main/java/com/example/catedu/FragmentQuesLink.java
+++ app/src/main/java/com/example/catedu/FragmentQuesLink.java
@@ -94,7 +94,7 @@ public class FragmentQuesLink extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ques_retrieval, container, false);
+        return inflater.inflate(R.layout.fragment_ques_link, container, false);
     }
 
     @Override
