7447487015c6a87a39994aff9de3302867e42854
ywt <yaowt19@mails.tsinghua.edu.cn>
2021-09-05 20:29:45 +0800

    naive chat room

--- app/src/main/java/com/example/catedu/FragmentQuesQa.java
+++ app/src/main/java/com/example/catedu/FragmentQuesQa.java
@@ -1,5 +1,8 @@
 package com.example.catedu;
 
+import static cn.jiguang.imui.commons.models.IMessage.MessageType.RECEIVE_TEXT;
+import static cn.jiguang.imui.commons.models.IMessage.MessageType.SEND_TEXT;
+
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -7,10 +10,21 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.os.Message;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
+
+import cn.jiguang.imui.commons.ImageLoader;
+import cn.jiguang.imui.commons.models.IMessage;
+import cn.jiguang.imui.commons.models.IUser;
+import cn.jiguang.imui.messages.MessageList;
+import cn.jiguang.imui.messages.MsgListAdapter;
+
+import java.util.UUID;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,34 +34,23 @@ import android.widget.ImageButton;
 public class FragmentQuesQa extends Fragment {
 
     ImageButton back_home;
+    MessageList messageList;
+    ImageLoader imageLoader;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    Button testBtn;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
 
     public FragmentQuesQa() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentQa.
-     */
+
     // TODO: Rename and change types and number of parameters
     public static FragmentQuesQa newInstance(String param1, String param2) {
         FragmentQuesQa fragment = new FragmentQuesQa();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -55,10 +58,6 @@ public class FragmentQuesQa extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -78,7 +77,131 @@ public class FragmentQuesQa extends Fragment {
                 throwable.printStackTrace();
             }
         });
+        messageList = view.findViewById(R.id.msg_list);
+//        MsgListAdapter adapter = new MsgListAdapter<>("0", holdersConfig, imageLoader);
+        MsgListAdapter adapter = new MsgListAdapter<>("0", imageLoader);
+        messageList.setAdapter(adapter);
+
+
+        testBtn = view.findViewById(R.id.btn_test);
+        testBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                adapter.addToStart(new MyMessage("测试提问", SEND_TEXT), true);
+                messageList.setAdapter(adapter);
+                try{
+                    Thread.sleep(1000);
+                }catch (Throwable e){}
+
+                adapter.addToStart(new MyMessage("测试回答", RECEIVE_TEXT), true);
+                messageList.setAdapter(adapter);
+
+            }
+        });
+
     }
+
+    public class MyMessage implements IMessage {
+
+        private long id;
+        private String text;
+        private String timeString;
+        private MessageType type;
+        private IUser user;
+        private String contentFile;
+        private long duration;
+
+        public MyMessage(String text, MessageType type) {
+            this.text = text;
+            this.type = type;
+            this.id = UUID.randomUUID().getLeastSignificantBits();
+        }
+
+        @Override
+        public String getMsgId() {
+            return String.valueOf(id);
+        }
+
+        @Override
+        public IUser getFromUser() {
+            if (user == null) {
+                return new DefaultUser("0", "user1", null);
+            }
+            return user;
+        }
+
+        public void setUserInfo(IUser user) {
+            this.user = user;
+        }
+
+        public void setMediaFilePath(String path) {
+            this.contentFile = path;
+        }
+
+        public void setDuration(long duration) {
+            this.duration = duration;
+        }
+
+        @Override
+        public long getDuration() {
+            return duration;
+        }
+
+        public void setTimeString(String timeString) {
+            this.timeString = timeString;
+        }
+
+        @Override
+        public String getTimeString() {
+            return timeString;
+        }
+
+        @Override
+        public MessageType getType() {
+            return type;
+        }
+
+        @Override
+        public String getText() {
+            return text;
+        }
+
+        @Override
+        public String getMediaFilePath() {
+            return contentFile;
+        }
+    }
+
+
+
+    public class DefaultUser implements IUser {
+
+        private String id;
+        private String displayName;
+        private String avatar;
+
+        public DefaultUser(String id, String displayName, String avatar) {
+            this.id = id;
+            this.displayName = displayName;
+            this.avatar = avatar;
+        }
+
+        @Override
+        public String getId() {
+            return id;
+        }
+
+        @Override
+        public String getDisplayName() {
+            return displayName;
+        }
+
+        @Override
+        public String getAvatarFilePath() {
+            return avatar;
+        }
+    }
+
 
     protected void forwardSwitchFragment() {
         int from = MainActivity.last_fragment, to = MainActivity.fragments.size() - 1;
