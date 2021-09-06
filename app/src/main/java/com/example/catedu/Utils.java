45f670c6cd914c5e644777f53e5e137d6801809d
周 <zhouyan19@mails.tsinghua.edu.cn>
2021-09-07 03:22:51 +0800

    OCR to be continued

--- app/src/main/java/com/example/catedu/Utils.java
+++ app/src/main/java/com/example/catedu/Utils.java
@@ -1,5 +1,11 @@
 package com.example.catedu;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
+
 public class Utils {
     static String English(String chi) {
         String eng = "";
@@ -73,5 +79,29 @@ public class Utils {
                 break;
         }
         return chi;
+    }
+
+    public static Bitmap stringToBitmap(String string) {
+        // 将字符串转换成Bitmap类型
+        Bitmap bitmap = null;
+        try {
+            byte[] bitmapArray;
+            bitmapArray = Base64.decode(string, Base64.DEFAULT);
+            bitmap = BitmapFactory.decodeByteArray(bitmapArray, 0,
+                    bitmapArray.length);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+
+    public static String bitmapToString(Bitmap bitmap){
+        //将Bitmap转换成字符串
+        String string = null;
+        ByteArrayOutputStream bStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG,100,bStream);
+        byte []bytes = bStream.toByteArray();
+        string = Base64.encodeToString(bytes,Base64.DEFAULT);
+        return string;
     }
 }
